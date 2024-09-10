c25eb40c8422fb5258a27c63571c0f2e3de660cd
Spike Lu <spike@trychroma.com>
2024-09-10 12:28:39 -0700

    remove unused arg

--- chromadb/api/models/AsyncCollection.py
+++ chromadb/api/models/AsyncCollection.py
@@ -267,7 +267,6 @@ class AsyncCollection(CollectionCommon["AsyncServerAPI"]):
             documents=documents,
             images=images,
             uris=uris,
-            require_data=False,
         )
 
         await self._client._update(
@@ -311,7 +310,6 @@ class AsyncCollection(CollectionCommon["AsyncServerAPI"]):
             documents=documents,
             images=images,
             uris=uris,
-            require_data=True,
         )
 
         await self._client._upsert(
